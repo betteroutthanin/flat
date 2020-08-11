2d0b36ccde70deb3349142db209bdd0ea6ed8d72
James Hall <betteroutthanin@gmail>
2020-08-11 16:57:17 +1000

    * Clean up formatting of floats

--- flat.py
+++ flat.py
@@ -1,4 +1,5 @@
 import datetime
+import math
 '''
 https://marlinfw.org/docs/gcode
 - Start at bottom left corner
@@ -14,12 +15,12 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
     le = "\n"
     b=  ""
     b += "(" + datetime.datetime.now().strftime("%Y/%B/%d/ at %H:%M:%S:%p") +")" + le
-    b += "(xDelta = " + str(xDelta) + ")" + le
-    b += "(yDelta = " + str(yDelta) + ")" + le
-    b += "(zDelta = " + str(zDelta) + ")" + le
-    b += "(zStepSize = " + str(zStepSize) + ")" + le
-    b += "(speed = " + str(speed) + ")" + le
-    b += "(feed = " + str(feed) + ")" + le    
+    b += "(xDelta = " + v(xDelta) + ")" + le
+    b += "(yDelta = " + v(yDelta) + ")" + le
+    b += "(zDelta = " + v(zDelta) + ")" + le
+    b += "(zStepSize = " + v(zStepSize) + ")" + le
+    b += "(speed = " + v(speed) + ")" + le
+    b += "(feed = " + v(feed) + ")" + le    
 
     # Note: zDelta + zStepSize are passed in as postive numbers
     # But: They need they need to be negative numbers to be aligned with the coord system
@@ -31,16 +32,16 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
     z = 0.0
         
     b += le + "(Starting Spindle)" + le
-    b += "M3 S" + str(speed) + le
+    b += "M3 S" + v(speed) + le
     
     b += "(Do nothing and allow the spindle up to speed)" + le
     b += "M0 S3" + le
   
     b += "(Set the master feed rate)" + le
-    b += "G01 F" + str(feed) + le
+    b += "G01 F" + v(feed) + le
     
     b += "(Lower the head to Z0 before we begin - half the feed rate)" + le
-    b += "G01 Z" + str(z) + " " + "F" + str(feed / 4) + le
+    b += "G01 Z" + v(z) + " " + "F" + v(feed / 4) + le
 
     # Main loopy bit
     zp = 1    
@@ -55,8 +56,8 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
 
         b += le + "(Starting a new x/y pass!)" + le
         b += "(Lower the head to to target z for this x/y pass - 1/4 rate)" + le
-        b += "(Target Z = " + str(z) + ")" + le
-        b += "G01 Z" + str(z) + " " + "F" + str(feed / 4) + le
+        b += "(Target Z = " + v(z) + ")" + le
+        b += "G01 Z" + v(z) + " " + "F" + v(feed / 4) + le
         
         lastPass = False
         yp = 1
@@ -68,7 +69,7 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
                  x = 0
 
             # move the head along the x
-            b += "G01 X" + str(x) + " Y" + str(y) + " Z" + str(z) + le            
+            b += "G01 X" + v(x) + " Y" + v(y) + " Z" + v(z) + le            
 
             if lastPass:
                 break 
@@ -81,14 +82,14 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
                 y = yDelta
                 lastPass = True
 
-            b += "G01 X" + str(x) + " Y" + str(y) + " Z" + str(z) + le            
+            b += "G01 X" + v(x) + " Y" + v(y) + " Z" + v(z) + le            
         # End while yp
         # Need to return to 0,0,2
         x = 0.0
         y = 0.0        
 
         b += "(Returning head for next pass)" + le
-        b += "G01 X" + str(x) + " Y" + str(y) + " Z" + str(2) + le            
+        b += "G01 X" + v(x) + " Y" + v(y) + " Z" + v(2) + le            
     # End while zp
 
     # Y boarder on each side?
@@ -103,4 +104,9 @@ def flat(xDelta, yDelta, zDelta, zStepSize, speed, feed, cutterWidth):
     f = open("flat.nc", "w")
     f.write(b)
     f.close()
+
+def v(value: float):    
+    b = str(value)
+    b = "{:.2f}".format(value)
+    return b
    
